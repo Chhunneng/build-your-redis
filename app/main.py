3f35058062cf7336697db98c67d2d62d25c861da
Chrea Chanchhunneng <chreachanchhunneng@gmail.com>
2024-05-25 20:11:29 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
 import socket
+import threading
+
+def handle_connection(conn, addr):
+    while True:
+        request = conn.recv(512)
+        if not request:
+            break
+        data = request.decode()
+        if "ping" in data.lower():
+            response = "+PONG\r\n"
+            conn.send(response.encode())
+    # conn.close()
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
-    client, _ = server_socket.accept()
-    # client.send(b"+PONG\r\n")
     while True:
-        request = client.recv(512)
-        data = request.decode()
-        if "ping" in data.lower():
-            client.send("+PONG\r\n".encode())
+        client_socket, client_address = server_socket.accept()
+        print(f"Received a connection from {client_address}")
+        handle_connection(client_socket, client_address)
+        threading.Thread(
+            target=handle_connection, args=[client_socket, client_address]
+        ).start()
 
 
 if __name__ == "__main__":
