3e0a299f64f16ba44535f8014abd0a03d849dfd0
Chrea Chanchhunneng <chreachanchhunneng@gmail.com>
2024-05-25 20:00:49 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,8 +4,13 @@ import socket
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     # server_socket.accept() # wait for client
-    client, addr = server_socket.accept()
-    client.send(b"+PONG\r\n")
+    client, _ = server_socket.accept()
+    # client.send(b"+PONG\r\n")
+    while True:
+        request = client.recv(512)
+        data = request.decode()
+        if "ping" in data.lower():
+            client.send("+PONG\r\n".encode())
 
 
 if __name__ == "__main__":
