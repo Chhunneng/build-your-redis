b9892ebb3fad1367c2be0428819d168993702f5c
Chrea Chanchhunneng <chreachanchhunneng@gmail.com>
2024-05-26 11:04:14 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,14 +4,16 @@ from threading import Thread
 from time import time
 from argparse import ArgumentParser
 
+data = {}
+expiry_time = {}
+
 
 class Connection(Thread):
     def __init__(self, socket, address):
         super().__init__()
         self.sock = socket
         self.addr = address
-        self.data = {}
-        self.expiry_time = {}
+
         print(f"Connection from {address}")
         self.start()
 
@@ -35,37 +37,37 @@ class Connection(Thread):
                 print(f"ECHOING! {req[1]}")
                 self.sock.send(f"+{req[1]}\r\n".encode())
             case "set":
-                self.data[req[1]] = req[2]
+                data[req[1]] = req[2]
                 if len(req) > 3 and req[3].lower() == "px":
                     additional_time = int(req[4])
-                    self.expiry_time[req[1]] = additional_time + (time() * 1000)
+                    expiry_time[req[1]] = additional_time + (time() * 1000)
                 self.sock.send("+OK\r\n".encode())
             case "get":
-                if req[1] in self.data and (
-                    (
-                        req[1] in self.expiry_time
-                        and self.expiry_time[req[1]] >= time() * 1000
-                    )
-                    or req[1] not in self.expiry_time
+                if req[1] in data and (
+                    (req[1] in expiry_time and expiry_time[req[1]] >= time() * 1000)
+                    or req[1] not in expiry_time
                 ):
-                    self.sock.send(f"+{self.data[req[1]]}\r\n".encode())
+                    self.sock.send(f"+{data[req[1]]}\r\n".encode())
                 else:
                     self.sock.send("$-1\r\n".encode())
             case "info":
                 if req[1].lower() == "replication":
-                    self.sock.send("$11\r\nrole:master\r\n".encode())
-
+                    self.sock.send(f"${5 + len(role)}\r\nrole:{role}\r\n".encode())
 
 
 def main():
     print("STARTED!")
-    parser = ArgumentParser("A Redis server written in Python")
-    parser.add_argument("--port", type=int, default=6379)
-    server_socket = socket.create_server(("localhost", parser.parse_args().port), reuse_port=True)
+    server_socket = socket.create_server(
+        ("localhost", parser.parse_args().port), reuse_port=True
+    )
     while True:
         client, client_addr = server_socket.accept()  # wait for client
         Connection(client, client_addr)
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser("A Redis server written in Python")
+    parser.add_argument("--port", type=int, default=6379)
+    parser.add_argument("--replicaof", nargs="+", help="Replica server host and port")
+    role = "master" if not parser.parse_args().replicaof else "slave"
     main()
